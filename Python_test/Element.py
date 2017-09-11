09a869cb7581b8120ed8022fada7192669e9048d
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-11 10:26:43 +0800

    naive update

--- Python_test/Element.py
+++ Python_test/Element.py
@@ -10,10 +10,10 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 # change encoding into utf-8 for correctly printing
 
 POINTS = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 101, 102]
-POINTS_SHOW = {3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10', 11: 'J',\
+POINTS_SHOW = {3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10', 11: 'J',
                12: 'Q', 13: 'K', 14: 'A', 15: '2', 101: 'Joker', 102: '\033[1;31mJoker\033[0m'}
-POINTS_FULL = {3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',\
-               10: 'ten', 11: 'jack', 12: 'queen', 13: 'king', 14: 'ace', 15: 'two',\
+POINTS_FULL = {3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
+               10: 'ten', 11: 'jack', 12: 'queen', 13: 'king', 14: 'ace', 15: 'two',
                101: 'joker', 102: '\033[1;31mjoker\033[0m'}
 SUITS = ['D', 'C', 'H', 'S']
 SUITS_SHOW = {'D': '\033[1;31m♦\033[0m', 'C': '♣', 'H': '\033[1;31m♥\033[0m', 'S': '♠'}
@@ -21,7 +21,10 @@ SUITS_FULL = {'D': 'diamond', 'C': 'club', 'H': 'heart', 'S': 'spade'}
 
 
 class Card:
-    """ Card class """
+    """
+    Card class
+    """
+
     def __init__(self, point, suit=None):
         if point not in POINTS:
             raise Exception('Point must in %s!' % str(POINTS))
@@ -41,16 +44,16 @@ class Card:
             raise Exception('Mode must in [SHOW, FULL]!')
 
         if mode == 'SHOW':
-            return POINTS_SHOW[self.point] if self.suit is None\
+            return POINTS_SHOW[self.point] if self.suit is None \
                 else SUITS_SHOW[self.suit] + POINTS_SHOW[self.point]
         if mode == 'FULL':
-            return POINTS_FULL[self.point] if self.suit is None\
+            return POINTS_FULL[self.point] if self.suit is None \
                 else 'the ' + POINTS_FULL[self.point] + ' of ' + SUITS_FULL[self.suit]
 
     def __eq__(self, other):
         """ Judge if two cards are equal """
         if not isinstance(other, Card):
-        	raise Exception('Must compare with Card!')
+            raise Exception('Must compare with Card!')
 
         return True if self.suit == other.suit and self.point == other.point else False
 
